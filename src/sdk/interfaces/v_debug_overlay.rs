d8247fc48fdc66e951016c3a6272223e54f3ef7a
Mariell Hoversholm <proximyst@proximyst.com>
2019-06-03 13:57:48 +0200

    Port iv_debug_overlay

---
+++ src/sdk/interfaces/v_debug_overlay.rs
@@ -0,0 +1,44 @@
+use std::{ffi::c_void, mem::{self, transmute}};
+
+use getset::Getters;
+
+use super::super::math::Vector3D;
+use super::super::prelude::*;
+
+#[derive(Debug, Copy, Clone, Getters)]
+#[get = "pub"]
+pub struct VDebugOverlayInterface {
+    inner: Interface,
+}
+
+#[repr(isize)]
+pub enum VDebugOverlayVTableIndicies {
+    WorldToScreen = 13,
+}
+
+impl ClientModeInterface {
+    pub const fn new(inner: Interface) -> Self {
+        ClientModeInterface { inner }
+    }
+
+    pub fn world_to_screen(
+        &self,
+        input: &Vector3D,
+    ) -> Result<Option<Vector3D>> {
+        type Func = unsafe extern "thiscall" fn(*const usize, *const Vector3D, *mut Vector3D) -> i32;
+
+        let mut vector: Vector3D = unsafe { mem::zeroed() };
+        let return_code = unsafe {
+            transmute::<_, Func>(
+                self.inner
+                    .nth(VDebugOverlayVTableIndicies::WorldToScreen as isize)?,
+            )(*self.inner.handle(), input as *const _, &mut vector as *mut _)
+        };
+
+        if return_code == 1 {
+            return Ok(None);
+        }
+
+        Ok(Some(vector))
+    }
+}
